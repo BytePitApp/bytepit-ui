5a5ab98c0cbae23494a8080e976cd7fad8525d6f
filipides1 <95239038+filipides1@users.noreply.github.com>
2023-11-05 22:49:02 +0100

    Revert "Added foundations for creating forms"

--- bytepit-ui/src/App.tsx
+++ bytepit-ui/src/App.tsx
@@ -1,29 +1,9 @@
-import { Form } from "./components/Form"
-import { FormComponentSpecification, FormTypes } from "./Models/Form"
-import { useState } from "react"
 
 function App() {
-    const [name, setName] = useState("")
-
-    const formConfigurations: FormComponentSpecification[] = [
-        {
-            label: "Name",
-            type: FormTypes.TEXT,
-            key: "name",
-            value: "",
-            styleClass: "bg-gray-500 p-2 rounded-md",
-            updateValue: setName,
-            required: true,
-            placeholder: "Enter your name",
-        },
-    ]
-
     return (
-        <div>
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
-            <Form props={formConfigurations} styleClass={""} />
-            <p>{name}</p>
-        </div>
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
     )
 }
 
