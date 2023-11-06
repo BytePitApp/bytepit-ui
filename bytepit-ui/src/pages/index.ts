1b7f7095284239e8b8e46967e59df6e7a57da60c
Filip Bernt <filip.bernt@gmail.com>
2023-11-06 15:09:38 +0100

    RADE-32 modified names of pages, added import-export to the pages and to index.ts

---
+++ bytepit-ui/src/pages/index.ts
@@ -0,0 +1,13 @@
+import { AdminHomePage, OrganiserHomePage, ContestantHomePage } from './HomePage';
+import LoginPage from './LoginPage';
+import RegisterPage from './RegisterPage';
+
+
+export {
+    AdminHomePage,
+    OrganiserHomePage,
+    ContestantHomePage,
+    LoginPage,
+    RegisterPage
+
+}
