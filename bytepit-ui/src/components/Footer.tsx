d08fa483a57448d23a741baff66a38d8207a1bd9
TeoMatosevic <93163183+TeoMatosevic@users.noreply.github.com>
2023-11-10 14:22:11 +0100

    Added footer component

---
+++ bytepit-ui/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="w-full p-4">
+            <h1 className="text-3xl font-bold underline">Footer</h1>
+        </div>
+    )
+}
+
+export default Footer
