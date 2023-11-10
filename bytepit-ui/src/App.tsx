e5708e3768a3d6e066f1f5ea5967b50893d8d79e
TeoMatosevic <93163183+TeoMatosevic@users.noreply.github.com>
2023-11-10 14:22:11 +0100

    Added footer component

--- bytepit-ui/src/App.tsx
+++ bytepit-ui/src/App.tsx
@@ -1,6 +1,7 @@
 import { Form } from "./components/Form"
 import { FormComponentSpecification, FormTypes } from "./Models/Form"
 import { useState } from "react"
+import { Footer } from "./components"
 
 function App() {
     const [name, setName] = useState("")
@@ -19,10 +20,13 @@ function App() {
     ]
 
     return (
-        <div>
+        <div className="static">
             <h1 className="text-3xl font-bold underline">Hello world!</h1>
             <Form props={formConfigurations} styleClass={""} />
             <p>{name}</p>
+            <div className="absolute bottom-0 bg-gray-400 w-full">
+                <Footer />
+            </div>
         </div>
     )
 }
