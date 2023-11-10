17bebc6c1011bfa176cafe97ba2662945cdb1a1c
Teo Radolovict <tr54427@fer.hr>
2023-11-10 16:03:23 +0100

    [RADE-35] - Separated svgs in /assets/svgs folder.

---
+++ bytepit-ui/src/assets/index.ts
@@ -0,0 +1,11 @@
+import DefaultUserImage from './images/default_user.png'
+import ReactSvg from './svgs/react.svg'
+import CloseMenuSvg from './svgs/closeMenu.svg'
+import HamburgerMenuSvg from './svgs/hamburgerMenu.svg'
+
+export {
+    DefaultUserImage,
+    ReactSvg,
+    CloseMenuSvg,
+    HamburgerMenuSvg,
+}
