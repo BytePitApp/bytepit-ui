6191acaadeb3b9e5cb42be22921fb3b0fe3b4a3b
Teo Radolovict <tr54427@fer.hr>
2023-11-16 00:48:09 +0100

    [RADE-51] - Added transform animation in landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,48 +2,54 @@ import { BubbleText, HoverCard, Navbar } from "../components"
 import { LandingBackground } from "../assets"
 import { IoTrophy, IoDesktopOutline, IoCalendar } from "react-icons/io5"
 import { Button } from "primereact/button"
+import { useEffect, useState } from "react"
 
 const LandingPage = () => {
+    const [val, setVal] = useState(false)
+    
+    useEffect(() => {
+        setVal(true)
+    }, [])
+
     return (
-        <div 
-            className="flex flex-col h-full bg-cover bg-center bg-no-repeate"
+        <div
+            className="flex h-full flex-col bg-cover bg-center bg-no-repeate"
             style={{backgroundImage: `url(${LandingBackground})`}}>
             <Navbar />
             <div className="flex w-full flex-grow">
-                <div className="w-[54.3rem] h-full flex flex-col items-center justify-between p-12 bg-primary/10 bg-opacity-70">
+                <div className="w-[45%] flex flex-col items-center justify-between p-10 bg-primary/10 bg-opacity-70">
                     <BubbleText />
-                    <div className="border-l-2 border-black p-5 flex flex-col gap-5 mb-32">
-                        <span className="text-xl text-black font-bold">
-                            Interested, start learning... Sign up for free now?
-                        </span>
+                    <div className="border-l-2 border-black p-5 flex flex-col gap-5 mb-[20%]">
+                        <div className="flex flex-col text-xl text-black font-bold">
+                            <p>Create free account now! Happy coding!</p>
+                        </div>
                         <Button label="Get Started" className="w-fit" />
                     </div>
                 </div>
-                <div className="flex-1 h-2/3 flex justify-center items-center mt-10">
-                <div className="grid w-4/5 gap-10 py-32 grid-flow-col overflow-x-auto">
+                <div className="flex-1 h-2/3 flex justify-center items-center mt-20">
+                    <div className="grid w-4/5 gap-10 py-32 grid-flow-col overflow-hidden">
                         <HoverCard
-                            className="translate-y-10"
+                            className={`${!val && "translate-x-[100vw]" } translate-y-20 transition-transform duration-700 delay-100`}
                             title="Code & Learn"
-                            subtitle="Solve coding problems and learn"
+                            subtitle="Browse through coding problems, create your own custom problems"
                             Icon={IoDesktopOutline}
                             href="/register"
                         />
                         <HoverCard
-                            className=""
+                            className={`${!val && "translate-x-[100vw]" } transition-transform duration-700 delay-300`}
                             title="Compete with others"
-                            subtitle="Participate in competitions with other contestants and become coding champ"
+                            subtitle="Participate in competitions against other contestants and become coding champ"
                             Icon={IoTrophy}
                             href="/register"
                         />
                         <HoverCard
-                            className="-translate-y-10"
-                            title="Custom coding problems and competitions"
-                            subtitle="Create cutom coding problems for real coding challenge and host competitions"
+                            className={`${!val && "translate-x-[100vw]" } -translate-y-20 transition-transform duration-700 delay-500`}
+                            title="Host competitions"
+                            subtitle="Create problems and publish them for real coding challenge"
                             Icon={IoCalendar}
                             href="/register"
                         />
                     </div>
-                
                 </div>
             </div>
         </div>
