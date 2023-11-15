ceac6612c5ed0ae0c148f4f12997735f457ce2f7
Jakov Vinožganić <yourgitemail@example.com>
2023-11-15 19:41:23 +0100

    Added footer and fixed up AdminHomePage

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,25 @@ module.exports = {
         "./components/**/*.{js,ts,jsx,tsx,mdx}",
         "./node_modules/primereact/**/*.{js,ts,jsx,tsx}",
         "./node_modules/primeicons/**/*.{js,ts,jsx,tsx}",
-        
 
         // Or if using `src` directory:
         "./src/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                "major-mono": ["Major Mono Display"],
+            },
+            colors: {
+                primary: "#4338CA",
+                primarylight: "#554acf",
+                secondary: "#2196F3",
+                secondarylight: "#3da2f5",
+                secondarydark: "#1976D2",
+                graymedium: "#f8f9fa",
+                graydark: "#dee2e6",
+            },
+        },
     },
     plugins: [],
 }
