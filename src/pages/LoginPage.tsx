c782d2ea800f15188ed67bdaf35d03c2f2272611
TeoMatosevic <93163183+TeoMatosevic@users.noreply.github.com>
2023-11-15 18:25:20 +0100

    Created register page and redesigned login page a bin.

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,31 +1,51 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 import { Button } from "primereact/button"
-import { ProgressSpinner } from "primereact/progressspinner";
+import { ProgressSpinner } from "primereact/progressspinner"
 import { useNavigate } from "react-router-dom"
 import axios from "axios"
 import { InputText } from "primereact/inputtext"
 import { Password } from "primereact/password"
 import { FormDataLogin } from "../Models"
+import { Messages } from "primereact/messages"
+import { useMountEffect } from "primereact/hooks"
+import uuid from "react-uuid"
 
 const LoginPage = () => {
     const [formData, setFormData] = useState<FormDataLogin>({
         username: "",
-        password: ""
+        password: "",
     })
-    const [error, setError] = useState<string>("")
     const [loading, setLoading] = useState(false)
+    const [id, setId] = useState<string>(uuid())
+    const msgs = useRef<Messages>(null)
+
+    useMountEffect(() => {
+        msgs.current?.clear()
+    })
 
     const navigate = useNavigate()
 
     const login = async () => {
         setLoading(true)
-        setError("")
         try {
-            const response = await axios.post("http://localhost:8000/auth/login",
+            if (formData.username === "" || formData.password === "") {
+                msgs.current?.show({
+                    id: id,
+                    severity: "error",
+                    summary: "Error!",
+                    detail: "Please fill in all fields!",
+                    closable: false,
+                })
+                setId(uuid())
+                setLoading(false)
+                return
+            }
+            const response = await axios.post(
+                "http://localhost:8000/auth/login",
                 new URLSearchParams({ username: formData.username, password: formData.password }),
                 {
-                    headers: { 
-                        ContentType: "application/x-www-form-urlencoded"
+                    headers: {
+                        ContentType: "application/x-www-form-urlencoded",
                     },
                     withCredentials: true,
                 }
@@ -33,44 +53,60 @@ const LoginPage = () => {
             navigate("/contestant/home")
         } catch (err: any) {
             setLoading(false)
-            setError(err.response.data.detail)
+            msgs.current?.show({
+                id: id,
+                severity: "error",
+                summary: "Error!",
+                detail: err.response.data.detail,
+                closable: false,
+            })
+            setId(uuid())
         }
     }
 
     return (
-        <div className="flex flex-col justify-center items-center h-screen">
-            <div className="flex flex-col gap-10 w-1/3">
-                <span className="text-4xl">Login</span>
-                <span className="p-float-label">
-                    <InputText
-                        value={formData.username} 
-                        onChange={(e) => setFormData({
-                            username: e.target.value,
-                            password: formData.password,
-                        })} 
-                        className="w-full"
-                    />
-                    <label htmlFor="in">Username</label>
-                </span>
-                <span className="p-float-label flex items-center">
-                    <Password 
-                        toggleMask={true}
-                        value={formData.password}
-                        onChange={(e) => setFormData({
-                            username: formData.username,
-                            password: e.target.value,
-                        })} 
-                        feedback={false} 
-                        className='w-full' 
-                        inputClassName="w-full"
-                    />
-                    <label htmlFor="in">Password</label>
-                </span>
-                <Button label="Submit" onClick={login} />
-                {loading && <ProgressSpinner />}
-                {error !== "" &&
-                    <span className="text-red-700 font-semibold text-clip">{error}</span>
-                }
+        <div className="relative">
+            <div className="flex absolute right-0 py-10 px-6 z-30">
+                <Messages ref={msgs} />
+            </div>
+            <div className="flex flex-col justify-center items-center h-screen">
+                <div className="flex flex-col gap-10 w-1/3">
+                    <span className="text-4xl">Login</span>
+                    <span className="p-float-label">
+                        <InputText
+                            value={formData.username}
+                            onChange={(e) =>
+                                setFormData({
+                                    username: e.target.value,
+                                    password: formData.password,
+                                })
+                            }
+                            className="w-full"
+                        />
+                        <label htmlFor="in">Username</label>
+                    </span>
+                    <span className="p-float-label flex items-center">
+                        <Password
+                            toggleMask={true}
+                            value={formData.password}
+                            onChange={(e) =>
+                                setFormData({
+                                    username: formData.username,
+                                    password: e.target.value,
+                                })
+                            }
+                            feedback={false}
+                            className="w-full"
+                            inputClassName="w-full"
+                        />
+                        <label htmlFor="in">Password</label>
+                    </span>
+                    <Button label="SUBMIT" onClick={login} />
+                    {loading && <ProgressSpinner />}
+                    <div className="flex justify-end">
+                        <Button label="REGISTER" onClick={() => navigate("/register")} text raised />
+                    </div>
+                </div>
             </div>
         </div>
     )
