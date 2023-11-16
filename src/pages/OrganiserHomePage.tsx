956dcd9e805a784ffb944c77ea1978304e06bd59
Jakov Vinožganić <jakov.vinozganic@fer.hr>
2023-11-17 00:27:20 +0100

    Added backgrounds on all home pagesž

--- src/pages/OrganiserHomePage.tsx
+++ src/pages/OrganiserHomePage.tsx
@@ -2,7 +2,7 @@ import { Navbar } from "../components"
 
 const OrganiserHomePage = () => {
     return (
-        <div>
+        <div className="bg-form bg-cover min-h-screen">
             <Navbar />
         </div>
     )
