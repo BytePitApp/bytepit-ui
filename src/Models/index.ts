e67fbef0a0d9d4122a7c45f967bf529a200a9bfd
TeoMatosevic <93163183+TeoMatosevic@users.noreply.github.com>
2023-11-16 12:05:29 +0100

    Changed role to enum

--- src/Models/index.ts
+++ src/Models/index.ts
@@ -2,10 +2,12 @@ import Role from "./Role"
 import FormDataLogin from "./FormDataLogin"
 import Auth from "./Auth"
 import FormDataRegister from "./FormDataRegister"
+import RegisterRole from "./RegisterRole"
 
 export {
     Role,
     type FormDataLogin,
     type Auth,
     type FormDataRegister,
+    RegisterRole,
 }
