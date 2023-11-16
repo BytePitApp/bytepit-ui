2949a7cf82cfa74127d4c8aefb39bac0c417b2d8
Jakov Vinožganić <yourgitemail@example.com>
2023-11-16 19:50:05 +0100

    Landing page and fixed navbar a bit

--- src/components/HoverCard.tsx
+++ src/components/HoverCard.tsx
@@ -1,3 +1,4 @@
+import { reverse } from "dns"
 import { IconType } from "react-icons"
 
 interface CardType {
@@ -6,22 +7,24 @@ interface CardType {
     Icon: IconType
     href?: string
     className?: string
+    reverse: boolean
 }
 
-const HoverCard = ({ title, subtitle, Icon, href, className }: CardType) => {
+const HoverCard = ({ title, subtitle, Icon, href, className, reverse }: CardType) => {
     return (
-        <a href={href} className={`${className} p-4 rounded-2xl border-2 border-slate-200 relative overflow-hidden group bg-white/80`}>
-            <div className="absolute inset-0 bg-gradient-to-r from-violet-600 to-indigo-600 translate-y-[100%] group-hover:translate-y-[0%] transition-transform duration-300" />
-            <Icon className="absolute z-10 -top-12 -right-12 text-9xl text-slate-100 group-hover:text-violet-400 group-hover:rotate-12 transition-transform duration-300" />
-            <Icon className="mb-2 text-2xl text-violet-600 group-hover:text-white transition-colors relative z-10 duration-300" />
-            <h3 className="font-medium text-lg text-slate-950 group-hover:text-white relative z-10 duration-300">
+        <a href={href} className={`${className} p-6 rounded-2xl relative overflow-hidden group bg-${reverse ? "indigo-600" : "white/80"} ${reverse && "bg-gradient-to-r from-indigo-600 to-primary"} select-none`}>
+            <div className={`absolute inset-0 bg-gradient-to-r ${reverse ? "from-white" : "from-primary"} ${reverse ? "to-white" : "to-indigo-600"} translate-y-[100%] group-hover:translate-y-[0%] transition-transform duration-300{}`} />
+            <Icon className={`absolute z-10 -top-12 -right-12 text-9xl ${reverse ? "text-primarylight" : "text-slate-100"} group-hover:text-${reverse ? "primary" : "white"} group-hover:rotate-12 transition-transform duration-300{}`} />
+            <Icon className={`mb-2 text-2xl text-${reverse ? "white" : "primary"} group-hover:text-${reverse ? "primary" : "white"} transition-colors relative z-10 duration-300{}`} />
+            <h3 className={`font-medium text-2xl text-${reverse ? "white" : "slate-950"} ${reverse ? "group-hover:text-black" : "group-hover:text-white"} relative z-10 duration-300{}`}>
                 {title}
             </h3>
-            <p className="text-slate-400 group-hover:text-violet-200 relative z-10 duration-300">
+            <p className={`text-${reverse ? "slate-100" : "slate-400"} ${reverse ? "group-hover:text-slate-400" : "group-hover:text-white"} relative z-10 duration-300{}`}>
                 {subtitle}
             </p>
         </a>
     )
 }
+
 
 export default HoverCard
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -9,60 +9,74 @@ const LandingPage = () => {
     const navigate = useNavigate()
 
     return (
-        <div
-            className="flex h-full flex-col bg-cover bg-top bg-no-repeate bg-form">
+        <div className="flex h-full flex-col bg-cover bg-top bg-no-repeate bg-form">
             <Navbar />
-            <div className="flex w-full flex-grow">
-                <div className="w-[40%] flex flex-col items-center justify-start">
-                    <div className="flex flex-col text-[7rem] select-none font-major-mono">
-                        <div className="flex">
-                            <p className="transition-all duration-200 ease-in-out hover:scale-110">B</p>
-                            <p className="transition-all duration-200 ease-in-out hover:scale-110">y</p>
-                            <p className="transition-all duration-200 ease-in-out hover:scale-110">t</p>
-                            <p className="transition-all duration-200 ease-in-out hover:scale-110">e</p>
-                        </div>
-                        <div className="flex">
-                            <p className="transition-all -translate-y-10 duration-200 ease-in-out hover:scale-110">P</p>
-                            <p className="transition-all -translate-y-10 duration-200 ease-in-out hover:scale-110">i</p>
-                            <p className="transition-all -translate-y-10 duration-200 ease-in-out hover:scale-110">t</p>
-                        </div>
-                    </div>
-                    <div className="flex w-full justify-end mb-[10%]">
-                        <Button onClick={() => navigate("/register")} label="Get Started" className="w-fit text-xl px-10 py-5" />
-                    </div>
-                </div>
-                <div className="flex-1 h-2/3 flex flex-col items-start">
-                    <div className="grid gap-5 py-32 px-20 grid-flow-col">
+            <div className="flex w-full flex-grow pl-64 overflow-hidden">
+                <div className="flex-1 flex flex-col items-start">
+                    <div className="grid gap-5 py-32 grid-flow-row justify-between align grow">
                         <HoverCard
-                            className="-translate-y-10 w-full"
+                            className="-translate-y-10 w-3/5 max-w-3/5"
                             title="Code & Learn"
                             subtitle="Browse through coding problems or create your own"
                             Icon={IoDesktopOutline}
-                            href="/register"
+                            reverse={false}
                         />
                         <HoverCard
-                            className="w-full"
+                            className="w-3/5  max-w-3/5 translate-x-[50%]"
                             title="Compete with others"
                             subtitle="Participate in competitions against other contestants and become coding champ"
                             Icon={IoTrophy}
-                            href="/register"
+                            reverse={true}
                         />
                         <HoverCard
-                            className="translate-y-10 w-full"
+                            className="translate-y-10 w-3/5"
                             title="Host competitions"
                             subtitle="Organise competition for real coding challenge"
                             Icon={IoCalendar}
-                            href="/register"
+                            reverse={false}
                         />
                     </div>
-                    <Button onClick={() => navigate("/register")} label="Get Started" className="w-fit text-xl px-10 py-5" />
-
                 </div>
-
+                <div className=" flex flex-col items-center justify-start flex-1">
+                    <div className="bg-secondary rounded-b-[13rem] p-8 pt-10 pl-16 bg-gradient-to-b from-slate-200 via-slate-100 to-white">
+                        <div className="flex flex-col text-[8rem] select-none font-major-mono mr-10">
+                            <div className="flex">
+                                <p className="transition-all duration-200 ease-in-out hover:scale-110 text-primary font-[900]">B</p>
+                                <p className="transition-all duration-200 ease-in-out hover:scale-110 text-primary font-[900]">y</p>
+                                <p className="transition-all duration-200 ease-in-out hover:scale-110 text-primary font-[900]">t</p>
+                                <p className="transition-all duration-200 ease-in-out hover:scale-110 text-primary font-[900]">e</p>
+                            </div>
+                            <div className="flex justify-end">
+                                <p className="transition-all -translate-y-10 duration-200 ease-in-out hover:scale-110 text-primary font-[900]">
+                                    P
+                                </p>
+                                <p className="transition-all -translate-y-10 duration-200 ease-in-out hover:scale-110 text-primary font-[900]">
+                                    i
+                                </p>
+                                <p className="transition-all -translate-y-10 duration-200 ease-in-out hover:scale-110 text-primary font-[900]">
+                                    t
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="p-10 flex flex-col gap-5 mt-[20%] rounded-l-full w-full h-64 ml-20 bg-gradient-to-r from-indigo-600 via-primary to-indigo-800 ">
+                        <div className="flex flex-col items-center justify-center h-full gap-8 mr-20">
+                            <div className="flex flex-col text-2xl text-white font-bold">
+                                <p className="font-semibold">Create free account now! Happy coding!</p>
+                            </div>
+                            <Button
+                                className="bg-graydark rounded-full text-primary mt-1 hover:bg-white hover:scale-[102%] transition-all ease-in-out duration-300"
+                                label="No account yet? Register now!"
+                                size="large"
+                                onClick={() => navigate("/register")}
+                                text
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
 }
 
 export default LandingPage
-
