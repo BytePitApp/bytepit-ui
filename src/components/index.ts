f1b95020e3a80ac4e508ca67f0f52f022b7436e9
Teo Radolovict <tr54427@fer.hr>
2023-11-16 17:25:03 +0100

    landing page

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,9 @@
 import Navbar from "./Navbar"
 import Footer from "./Footer"
-import BubbleText from "./BubbleText"
 import HoverCard from "./HoverCard"
 
 export { 
     Navbar,
     Footer,
-    BubbleText,
     HoverCard,
 }
