7459daef9cfa02190d2f8ea8787991cf9bb03aeb
Teo Radolovict <tr54427@fer.hr>
2023-11-16 17:25:03 +0100

    landing page

--- src/components/HoverCard.tsx
+++ src/components/HoverCard.tsx
@@ -10,7 +10,7 @@ interface CardType {
 
 const HoverCard = ({ title, subtitle, Icon, href, className }: CardType) => {
     return (
-        <a href={href} className={`${className} w-full p-4 rounded-lg border-2 border-slate-200 relative overflow-hidden group bg-white/80`}>
+        <a href={href} className={`${className} p-4 rounded-2xl border-2 border-slate-200 relative overflow-hidden group bg-white/80`}>
             <div className="absolute inset-0 bg-gradient-to-r from-violet-600 to-indigo-600 translate-y-[100%] group-hover:translate-y-[0%] transition-transform duration-300" />
             <Icon className="absolute z-10 -top-12 -right-12 text-9xl text-slate-100 group-hover:text-violet-400 group-hover:rotate-12 transition-transform duration-300" />
             <Icon className="mb-2 text-2xl text-violet-600 group-hover:text-white transition-colors relative z-10 duration-300" />
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,4 +1,4 @@
-import { BubbleText, HoverCard, Navbar } from "../components"
+import { HoverCard, Navbar } from "../components"
 import { LandingBackground } from "../assets"
 import { IoTrophy, IoDesktopOutline, IoCalendar } from "react-icons/io5"
 import { Button } from "primereact/button"
@@ -6,53 +6,60 @@ import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 
 const LandingPage = () => {
-    const [val, setVal] = useState(false)
     const navigate = useNavigate()
-    
-    useEffect(() => {
-        setVal(true)
-    }, [])
 
     return (
         <div
-            className="flex h-full flex-col bg-cover bg-center bg-no-repeate"
+            className="flex h-full flex-col bg-cover bg-top bg-no-repeate"
             style={{backgroundImage: `url(${LandingBackground})`}}>
             <Navbar />
             <div className="flex w-full flex-grow">
-                <div className="w-[45%] flex flex-col items-center justify-between p-10 bg-primary/10 bg-opacity-70">
-                    <BubbleText />
-                    <div className="border-l-2 border-black p-5 flex flex-col gap-5 mb-[30%]">
-                        <div className="flex flex-col text-xl text-black font-bold">
-                            <p>Create free account now! Happy coding!</p>
+                <div className="w-[40%] flex flex-col items-center justify-start">
+                    <div className="flex flex-col text-[7rem] select-none font-major-mono">
+                        <div className="flex">
+                            <p className="transition-all duration-200 ease-in-out hover:scale-110">B</p>
+                            <p className="transition-all duration-200 ease-in-out hover:scale-110">y</p>
+                            <p className="transition-all duration-200 ease-in-out hover:scale-110">t</p>
+                            <p className="transition-all duration-200 ease-in-out hover:scale-110">e</p>
                         </div>
-                        <Button onClick={() => navigate("/register")} label="Get Started" className="w-fit" />
+                        <div className="flex">
+                            <p className="transition-all -translate-y-10 duration-200 ease-in-out hover:scale-110">P</p>
+                            <p className="transition-all -translate-y-10 duration-200 ease-in-out hover:scale-110">i</p>
+                            <p className="transition-all -translate-y-10 duration-200 ease-in-out hover:scale-110">t</p>
+                        </div>
+                    </div>
+                    <div className="flex w-full justify-end mb-[10%]">
+                        <Button onClick={() => navigate("/register")} label="Get Started" className="w-fit text-xl px-10 py-5" />
                     </div>
                 </div>
-                <div className="flex-1 h-2/3 flex justify-center items-center mt-20">
-                    <div className="grid gap-10 py-32 px-20 grid-flow-col overflow-hidden">
+                <div className="flex-1 h-2/3 flex flex-col items-start">
+                    <div className="grid gap-5 py-32 px-20 grid-flow-col">
                         <HoverCard
-                            className={`${!val && "translate-x-[100vw]" } translate-y-20 transition-transform duration-700 delay-100`}
+                            className="-translate-y-10 w-full"
                             title="Code & Learn"
-                            subtitle="Browse through coding problems, create your own custom problems "
+                            subtitle="Browse through coding problems or create your own"
                             Icon={IoDesktopOutline}
                             href="/register"
                         />
                         <HoverCard
-                            className={`${!val && "translate-x-[100vw]" } transition-transform duration-700 delay-300`}
+                            className="w-full"
                             title="Compete with others"
                             subtitle="Participate in competitions against other contestants and become coding champ"
                             Icon={IoTrophy}
                             href="/register"
                         />
                         <HoverCard
-                            className={`${!val && "translate-x-[100vw]" } -translate-y-20 transition-transform duration-700 delay-500`}
+                            className="translate-y-10 w-full"
                             title="Host competitions"
                             subtitle="Organise competition for real coding challenge"
                             Icon={IoCalendar}
                             href="/register"
                         />
                     </div>
+                    <Button onClick={() => navigate("/register")} label="Get Started" className="w-fit text-xl px-10 py-5" />
+
                 </div>
+
             </div>
         </div>
     )
