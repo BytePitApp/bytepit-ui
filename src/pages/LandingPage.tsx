f6ece6a1319be7c1388c8ab5865e2047c6396bf8
Teo Radolovict <tr54427@fer.hr>
2023-11-16 11:36:47 +0100

    [RADE-51] - landing page minimal changes

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,9 +3,11 @@ import { LandingBackground } from "../assets"
 import { IoTrophy, IoDesktopOutline, IoCalendar } from "react-icons/io5"
 import { Button } from "primereact/button"
 import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 const LandingPage = () => {
     const [val, setVal] = useState(false)
+    const navigate = useNavigate()
     
     useEffect(() => {
         setVal(true)
@@ -19,19 +21,19 @@ const LandingPage = () => {
             <div className="flex w-full flex-grow">
                 <div className="w-[45%] flex flex-col items-center justify-between p-10 bg-primary/10 bg-opacity-70">
                     <BubbleText />
-                    <div className="border-l-2 border-black p-5 flex flex-col gap-5 mb-[20%]">
+                    <div className="border-l-2 border-black p-5 flex flex-col gap-5 mb-[30%]">
                         <div className="flex flex-col text-xl text-black font-bold">
                             <p>Create free account now! Happy coding!</p>
                         </div>
-                        <Button label="Get Started" className="w-fit" />
+                        <Button onClick={() => navigate("/register")} label="Get Started" className="w-fit" />
                     </div>
                 </div>
                 <div className="flex-1 h-2/3 flex justify-center items-center mt-20">
-                    <div className="grid w-4/5 gap-10 py-32 grid-flow-col overflow-hidden">
+                    <div className="grid gap-10 py-32 px-20 grid-flow-col overflow-hidden">
                         <HoverCard
                             className={`${!val && "translate-x-[100vw]" } translate-y-20 transition-transform duration-700 delay-100`}
                             title="Code & Learn"
-                            subtitle="Browse through coding problems, create your own custom problems"
+                            subtitle="Browse through coding problems, create your own custom problems "
                             Icon={IoDesktopOutline}
                             href="/register"
                         />
@@ -45,7 +47,7 @@ const LandingPage = () => {
                         <HoverCard
                             className={`${!val && "translate-x-[100vw]" } -translate-y-20 transition-transform duration-700 delay-500`}
                             title="Host competitions"
-                            subtitle="Create problems and publish them for real coding challenge"
+                            subtitle="Organise competition for real coding challenge"
                             Icon={IoCalendar}
                             href="/register"
                         />
