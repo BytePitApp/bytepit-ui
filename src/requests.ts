8a0d73dbcdd2cd235796297902c1c0209c83069f
Marko Bolt <marko.bolt@fer.hr>
2023-11-17 18:03:07 +0100

    typo retardation fix

--- src/requests.ts
+++ src/requests.ts
@@ -1,9 +1,6 @@
 import axios from "axios"
 
-const url =
-    process.env.NODE_ENV === "production"
-        ? "https://bytepit.comhttps://bytepit-api.azurewebsites.net/"
-        : "http://localhost:8000"
+const url = process.env.NODE_ENV === "production" ? "https://bytepit-api.azurewebsites.net/" : "http://localhost:8000"
 
 const instance = axios.create({
     baseURL: url,
