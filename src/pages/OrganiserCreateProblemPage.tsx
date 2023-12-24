e76988ab6d5873935a401e5bcde2724097b2c27f
Filip Bernt <filip.bernt@gmail.com>
2023-12-24 12:34:22 +0100

    added organiser create problem page

---
+++ src/pages/OrganiserCreateProblemPage.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "../components"
+
+const OrganiserCreateProblemPage = () => {
+    return (
+        <div className="bg-form bg-cover min-h-screen">
+            <Navbar />
+        </div>
+    )
+}
+
+export default OrganiserCreateProblemPage
