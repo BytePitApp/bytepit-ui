84aacf86f9927495925182b91ab86cbd8d5f9f53
Marko Bolt <marko.bolt@fer.hr>
2023-12-27 15:48:22 +0100

    reverted test

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -38,7 +38,7 @@ const LandingPage = () => {
                         <div className="flex flex-col   text-[10vh] select-none font-major-mono mr-4 2xl:mr-10">
                             <div className="flex">
                                 <p className="transition-all duration-200 ease-in-out hover:scale-110 text-primary font-[900]">
-                                    BA
+                                    B
                                 </p>
                                 <p className="transition-all duration-200 ease-in-out hover:scale-110 text-primary font-[900]">
                                     y
