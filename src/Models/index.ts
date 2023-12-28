3e8f0310411fd859d1924baf6f039d0c714a5d6b
TeoMatosevic <93163183+TeoMatosevic@users.noreply.github.com>
2023-12-28 19:17:11 +0100

    Refactor

--- src/Models/index.ts
+++ src/Models/index.ts
@@ -3,6 +3,9 @@ import FormDataLogin from "./FormDataLogin"
 import Auth from "./Auth"
 import FormDataRegister from "./FormDataRegister"
 import RegisterRole from "./RegisterRole"
+import Problem from "./Problem"
+import ProblemPickerProps from "./ProblemPickerProps"
+import ProblemListItemProps from "./ProblemListItemProps"
 import Competition from "./Competition"
 import FormDataCompetition from "./FormDataCompetition"
 
@@ -12,6 +15,9 @@ export {
     type Auth,
     type FormDataRegister,
     RegisterRole,
+    type Problem,
+    type ProblemPickerProps,
+    type ProblemListItemProps,
     type Competition,
     type FormDataCompetition,
 }
