72f15b8854e5d0663e5aaf2a950f19f2730a8f0e
TeoMatosevic <93163183+TeoMatosevic@users.noreply.github.com>
2024-01-11 22:08:35 +0100

    Merged problem service

--- src/services/problem.service.ts
+++ src/services/problem.service.ts
@@ -1,5 +1,5 @@
 import requests from "../requests"
-import { CreateSubmission } from "../Models"
+import { CreateSubmission, CreateProblem, ModifyProblem } from "../Models"
 
 const getAllProblems = async () => {
     const response = await requests.get("/problems")
@@ -8,6 +8,53 @@ const getAllProblems = async () => {
 
 const getProblem = async (id: string) => {
     const response = await requests.get(`/problems/${id}`)
+    return response
+}
+
+const createProblem = async (problem: CreateProblem) => {
+    const formData = new FormData()
+
+    formData.append("name", problem.name)
+    formData.append("num_of_points", problem.points.toString())
+    formData.append("description", problem.description)
+    formData.append("runtime_limit", problem.runtimeLimit.toString())
+    formData.append("example_input", problem.exampleInput)
+    formData.append("example_output", problem.exampleOutput)
+    formData.append("is_private", problem.isPrivate.toString())
+    problem.testFiles?.forEach((file) => {
+        formData.append("test_files", file)
+    })
+    formData.append("is_hidden", problem.isHidden.toString())
+
+    const response = await requests.post("/problems", formData, {
+        headers: {
+            ContentType: "multipart/form-data",
+        },
+    })
+    return response
+}
+
+const modifyProblem = async (problem_id: string, problem: ModifyProblem) => {
+    const formData = new FormData()
+
+    formData.append("name", problem.name?.toString() || "")
+    formData.append("description", problem.description?.toString() || "")
+    formData.append("num_of_points", problem.points?.toString() || "")
+    formData.append("runtime_limit", problem.runtimeLimit?.toString() || "")
+    formData.append("example_input", problem.exampleInput?.toString() || "")
+    formData.append("example_output", problem.exampleOutput?.toString() || "")
+    formData.append("is_private", problem.isPrivate?.toString() || "")
+    problem.testFiles?.forEach((file) => {
+        formData.append("test_files", file)
+    })
+    formData.append("is_hidden", problem.isHidden?.toString() || "")
+
+    const response = await requests.patch(`/problems/${problem_id}`, formData, {
+        headers: {
+            ContentType: "multipart/form-data",
+        },
+    })
+
     return response
 }
 
@@ -27,4 +74,9 @@ const createSubmission = async (submission: CreateSubmission) => {
     return response
 }
 
-export { getAllProblems, createSubmission, getProblem }
+const getProblemsForOrganiser = async (id: string | undefined) => {
+    const response = await requests.get(`/problems/problems-by-organiser/${id}`)
+    return response
+}
+
+export { getAllProblems, createSubmission, getProblem, createProblem, modifyProblem, getProblemsForOrganiser }
